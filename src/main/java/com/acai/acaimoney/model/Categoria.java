a770a1743768a8f20ea90882d47009a699173d59
Juarez Alves Guimaraes <juarez.alves@bmtdes039.socic.lan>
2019-07-02 17:01:35 -0300

    Inclusão de classes e interfaces da entidade Categoria

---
+++ src/main/java/com/acai/acaimoney/model/Categoria.java
@@ -0,0 +1,27 @@
+package com.acai.acaimoney.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String nome;
+	public Long getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}	
+
+}
---
+++ src/main/java/com/acai/acaimoney/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.acai.acaimoney.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.acai.acaimoney.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long>{
+
+}
