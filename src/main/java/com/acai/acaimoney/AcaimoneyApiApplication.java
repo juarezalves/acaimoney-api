19ffb7832c3ccfe2e0edc0a8182d585a1a3342d5
Juarez Alves Guimaraes <juarez.alves@bmtdes039.socic.lan>
2019-07-02 14:18:47 -0300

    Criação do projeto

---
+++ src/main/java/com/acai/acaimoney/AcaimoneyApiApplication.java
@@ -0,0 +1,13 @@
+package com.acai.acaimoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaimoneyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaimoneyApiApplication.class, args);
+	}
+
+}
